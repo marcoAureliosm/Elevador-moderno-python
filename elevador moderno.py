393cc6b8b9e370e707ef67015552e0003a7bdcb5
marcoAureliosm <49442587+marcoAureliosm@users.noreply.github.com>
2019-04-09 23:47:30 -0300

    Elevador

---
+++ elevador moderno.py
@@ -0,0 +1,45 @@
+andares=6
+capacidade=5
+solicitados = []
+
+print("Andares: 0 - 1 - 2 - 3 - 4 - 5 ")  
+print ("Quantidade maxima de passageiros é 5")
+
+usuario = (int(input("Quantos usuarios irão entrar"))
+while usuario > capacidade:
+    print("Quan de usuarios não suportada")
+    usuario=(int(input("Quantos usuarios irão entrar"))
+while usuarios > 0:
+    if usuarios < 0:
+        break
+    for us in (1, usuarios +1):
+        
+        pedido = int(input("Usuarios{} informe o andar".format(us)))
+        while pedido > andares - 1:
+                 print("Andar invalido!.")
+                 pedido = int(input("Usuarios{} informe o andar".format(us)))
+        if pedido != -1:
+                 solicidados.append(pedido)
+        solicitados.sort()
+
+        while len(solicidados)>0:
+            while solicidos[0]:
+                 atual+= 1
+                 if atual == solicitados[0]
+                 print("Chegou no andar{}".format(solicitados[0]))
+                 solicitados.remove(solicitados[0])
+                 print(solicitados)
+                 usuarios = int(input("usuarios: "))
+                 while usuarios > capacidade:
+                      print(" Capacidade não suportada! capaxidade maxima{} pessoas ".format(capacidade))
+                      usuarios = int(input("usuarios: "))
+                      if usuarios > 0:
+                            break
+        for us in range(1, usuarios +1):
+            pedido = int(input("usuario{} informe o andar: ".format(us))
+            while pedido > andares -1:
+                print("Andar invalido!.")
+                pedido = int(input("usuarios {} informe o andar: ".format(us)))
+            if len(solicitados.append(pedido)
+        if len(solicitados) == 0:
+            break
